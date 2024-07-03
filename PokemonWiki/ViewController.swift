fed1c6299d13ee5d5afe50a8eaa4c63dd0196f1c
ElvisCtf <elvischengtf@gmail.com>
2024-07-03 10:22:21 +0800

    Initial Commit

---
+++ PokemonWiki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonWiki
+//
+//  Created by Elvis Cheng (ESD - Software Trainee, Digital Solutions) on 3/7/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
